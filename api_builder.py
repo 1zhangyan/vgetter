4c75f03c2aff385ff334d4c1e781d3d9c7e8e7f1
zhangyanbj06 <zhangyanbj06@yuanfudao.com>
2021-11-24 16:47:36 +0800

    ADD:add demo for it.

---
+++ api_builder.py
@@ -0,0 +1,122 @@
+import json
+from os import times
+import requests
+import math
+from random import choice
+from alive_progress import alive_bar
+
+def get_user_agent():
+    user_agents = ["Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+       "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+       "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+       "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+       "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+       "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+       "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+       "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+       "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+       "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+       "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+       "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+       "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+       "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+       "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+       "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+       "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+       "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+       "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+       "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+       "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+       "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+       "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+       "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+       "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+       "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+       "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+       "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+       "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+       "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36",
+       "Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"]
+    user_agent = choice(user_agents)
+    return user_agent
+
+def json_transfor(json_str):
+    try:
+        return json.loads(json_str.encode("utf-8").decode("latin1"))
+    except:
+        print("ERROR JSON:")
+        print(json_str)
+
+def send_and_get_http(url, headers):
+    try:
+        return requests.get(url, headers=headers).text
+    except:
+        print("EORROR URL")
+        print(headers)
+
+def get_http_header():
+    headers = {'Accept': '*/*',
+    'Accept-Language': 'zh-CN,zh;q=0.9'}
+    headers['User-Agent'] = get_user_agent()
+    return headers
+    
+def get_video_total_num(mid):
+    url = "https://api.bilibili.com/x/space/navnum?mid=" + mid
+    headers = get_http_header()
+    video_info = json_transfor(send_and_get_http(url, headers=headers))
+    return video_info['data']['video']
+
+def video_info_filter():
+    return True
+
+def get_cid_list(bvid):
+    url = "https://api.bilibili.com/x/player/pagelist?bvid=" + bvid
+    headers = get_http_header()
+    video_info = json_transfor(send_and_get_http(url, headers=headers))
+    need_vedio_list = filter(video_info_filter, video_info)
+    return list(map(lambda it: it['cid'],video_info['data']))
+    
+
+def get_user_cid_bvid(mid):
+    cid_bvid = {}
+    page_size = 30
+    page_total = math.ceil(get_video_total_num(mid)/page_size)
+    for page_num in range(page_total):
+        url = "https://api.bilibili.com/x/space/arc/search?mid={0}&ps={1}&tid=0&pn={2}".format(mid, str(page_size),str(page_num+1))
+        page_video_info = json_transfor(send_and_get_http(url, headers = get_http_header()))
+        for video_info in page_video_info['data']['list']['vlist']:
+            bvid = video_info['bvid']
+            for cid in get_cid_list(bvid):
+                cid_bvid[str(cid)] = bvid
+    return cid_bvid
+
+def download_single_video(bvid, order, url, size):
+    headers = get_http_header()
+    headers['referer'] = 'https://www.bilibili.com'
+    print("Start downloading" + bvid )
+    chunk_total = math.ceil(size/512)
+    r = requests.get(url,headers = headers, stream = True) 
+    f = open(bvid + str(order) + ".flv", "wb")
+    with alive_bar(chunk_total) as bar:
+        for chunk in r.iter_content(chunk_size = 512):
+            if chunk:
+                f.write(chunk)
+                bar()
+         
+def video_download(bvid, cid):
+    bvid_info = "bvid=" + bvid
+    cid_info = "cid=" + cid
+    url = "http://api.bilibili.com/x/player/playurl?qn=64&fnval=0&fnver=0&fourk=1&" + bvid_info + "&"+cid_info
+    headers = get_http_header()
+    headers['SESSDATA'] = 'xxx'
+    video_download_info = json_transfor(send_and_get_http(url, headers =headers ))
+    durl_list = video_download_info['data']['durl']
+    for durl in durl_list:
+        download_single_video(bvid, durl['order'], durl['url'], durl['size'])
